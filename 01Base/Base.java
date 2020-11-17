d61e52e4f276e35b1096d808083f8ad1bdd1e275
physcrowley <crowlda@ecolecatholique.ca>
2020-11-17 11:33:30 -0500

    add basic example

---
+++ 01Base/Base.java
@@ -0,0 +1,46 @@
+// Ajouter la définition pour notre objet Scanner
+import java.util.Scanner;
+
+class Base
+{
+    public static void main(String[] args) 
+    {
+        // Il faut déclarer le TYPE des variables
+        // Format : <type> <nom> = <valeur assignée>;
+
+        String s = "45"; // texte -> usage général
+        int n = 3; // nombres entiers -> pour compter
+        double d = 3.1415; // nombres décimaux -> pour les calculs
+
+        // conversions de type
+        n = Integer.parseInt(s);
+        d = Double.parseDouble(s);
+
+        // casting entre types numériques
+
+        n = (int) d;
+        d = n;
+
+        // SORTIE AU CONSOLE
+        // System.out est la console définit par ton système d'exploitation
+
+        System.out.println(s); // .println() ajoute automatiquement un retour de ligne
+
+        System.out.print(s); //.print() affiche seulement le texte (sans retour de ligne)
+
+        
+        // ENTRÉES AU CONSOLE
+        // Tu dois créer un objet de type Scanner
+        // System.in est aussi la console définit par ton système d'exploitation
+        Scanner input = new Scanner(System.in);
+
+        // Indiquer à l'utilisateur d'entreer quelque chose
+        System.out.println("Taper un nombre décimal ('.' est le symbole décimal)");
+        String getNum = input.nextLine(); // .nextLine() est la méthode pour capter une ligne de texte à la console
+        double num = Double.parseDouble(getNum);
+
+        System.out.println("Tu as inscrit le nombre " + num);
+
+        input.close(); // fermer + détruire le Scanner sur la console (System.in)
+    }
+}
---
+++ 01Base/base.py
@@ -0,0 +1,7 @@
+s = "45"
+n = 3
+d = 3.1415
+
+# légal
+n = s # n devient du texte (était un entier)
+n = int(s) # conversion de type
