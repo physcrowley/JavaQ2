f5b7afe856f8c3953745041d0f0e8d7591dfe3b5
physcrowley <crowlda@ecolecatholique.ca>
2020-11-17 11:33:40 -0500

    add structure example

---
+++ 0Structure/Structure.java
@@ -0,0 +1,22 @@
+/* Un commentaire de bloc
+
+EN-TÊTE : petite description
+
+Par : David Crowley, crowlda@ecolecatholique.ca
+*/
+
+/** Un commentaire de documentation : les classes, les méthodes, les attributs
+ * <p>  
+ * Classe qui a le même nom que le fichier. Les classes commencentent avec des majuscules */
+class Structure
+{
+    /** Tout programme doit avoir une méthode main
+     * <p>
+     * C'est le point d'entrée pour le programme. Java le cherche spécifiquement
+     * @param args - des options de la ligne de commande - pas utilisé ici
+     */
+    public static void main(String[] args)
+    {
+        // commentaire de ligne qui explique la logique du code
+    }
+}
