2a58f3e6f8d0d57f91f2218f58d0240830626cc2
physcrowley <crowlda@ecolecatholique.ca>
2020-11-27 11:29:07 -0500

    added extra messages to console

--- 03GestionDuFlux/CompareStrings.java
+++ 03GestionDuFlux/CompareStrings.java
@@ -4,12 +4,12 @@ public class CompareStrings {
       String inputFromScanner = "DroITe";
       
       String choix = inputFromScanner.toLowerCase(); //input.nextLine().toLowerCase();
-      
+      System.out.println("choix : " + choix);
       
       // == compare si les deux objets String sont les mêmes
       // objets en mémoire... compare des adresses, pas le texte dans l'objet
       
-      System.out.println("Compare with ==\n\t--> not recommended");
+      System.out.println("Compare with == : choix == \"droite\"\n\t--> not recommended");
       System.out.println(choix == "droite"); // false
       
       
@@ -18,7 +18,7 @@ public class CompareStrings {
       // 
       // la méthode s1.equalsIgnoreCase(s2) compare le texte en ignorant la case
        
-      System.out.println("\nCompare with .equals() or .equalsIgnoreCase()\n\t-->recommanded");
+      System.out.println("\nCompare with .equals() or .equalsIgnoreCase() : choix.equals(\"droite\")\n\t-->recommanded");
       System.out.println(choix.equals("droite")); // true
       
     }
