a1caf748ba38f0fc105b1d975ac1619702267c7c
physcrowley <crowlda@ecolecatholique.ca>
2020-11-27 11:26:13 -0500

    added CompareStrings.java example

---
+++ 03GestionDuFlux/CompareStrings.java
@@ -0,0 +1,25 @@
+public class CompareStrings {
+    public static void main(String[] args) {
+      // exemples de réponse saisie via le Scanner (p.ex. input)
+      String inputFromScanner = "DroITe";
+      
+      String choix = inputFromScanner.toLowerCase(); //input.nextLine().toLowerCase();
+      
+      
+      // == compare si les deux objets String sont les mêmes
+      // objets en mémoire... compare des adresses, pas le texte dans l'objet
+      
+      System.out.println("Compare with ==\n\t--> not recommended");
+      System.out.println(choix == "droite"); // false
+      
+      
+      // POUR COMPARER DU TEXTE, VOUS DEVRIEZ PLUTÔT UTILISER
+      // la méthode s1.equals(s2) compare le texte dans les objets String
+      // 
+      // la méthode s1.equalsIgnoreCase(s2) compare le texte en ignorant la case
+       
+      System.out.println("\nCompare with .equals() or .equalsIgnoreCase()\n\t-->recommanded");
+      System.out.println(choix.equals("droite")); // true
+      
+    }
+}
