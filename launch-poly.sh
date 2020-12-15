274ef2b9fd3ee300fcd900946f92cbb94bd0c9df
physcrowley <crowlda@ecolecatholique.ca>
2020-12-15 12:37:43 -0500

    updated launch-poly bash file with all java files

--- launch-poly.sh
+++ launch-poly.sh
@@ -1,2 +1,2 @@
-javac -d bin src/edu/ics4u/poly/Polymorphisme.java
+javac -d bin src/edu/ics4u/poly/FaireUnSon.java src/edu/ics4u/poly/Bark.java src/edu/ics4u/poly/Meow.java src/edu/ics4u/poly/Sing.java src/edu/ics4u/poly/Silence.java src/edu/ics4u/poly/Animal.java src/edu/ics4u/poly/Bird.java src/edu/ics4u/poly/Cat.java src/edu/ics4u/poly/Dog.java src/edu/ics4u/poly/Polymorphisme.java
 java -cp bin edu.ics4u.poly.Polymorphisme
